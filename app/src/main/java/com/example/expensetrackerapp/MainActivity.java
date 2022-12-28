2a90c40ad10daec414cefcc52db31976ff83a535
saroven <ashah3562@gmail.com>
2022-12-29 00:48:38 +0600

    add pages

--- app/src/main/java/com/example/expensetrackerapp/MainActivity.java
+++ app/src/main/java/com/example/expensetrackerapp/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.expensetrackerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener{
+
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(android.view.View view) {
+
     }
 }
