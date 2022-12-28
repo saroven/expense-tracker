3910e7b9fe0a6e8de32a99b29cf6c576fd302e5a
saroven <ashah3562@gmail.com>
2022-12-29 00:23:26 +0600

    add inital file

---
+++ app/src/main/java/com/example/expensetrackerapp/expense_lists.java
@@ -0,0 +1,15 @@
+package com.example.expensetrackerapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class expense_lists extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
