0beb836413162fd22d71d18c424d3f3bc256ab27
saroven <ashah3562@gmail.com>
2022-12-29 00:41:33 +0600

    add pages

---
+++ app/src/main/java/com/example/expensetrackerapp/AllIncome.java
@@ -0,0 +1,14 @@
+package com.example.expensetrackerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllIncome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_income);
+    }
+}
---
+++ app/src/main/java/com/example/expensetrackerapp/TotalBalance.java
@@ -0,0 +1,14 @@
+package com.example.expensetrackerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TotalBalance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_total_balance);
+    }
+}
